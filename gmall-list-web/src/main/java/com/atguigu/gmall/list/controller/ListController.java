c8604d223617777a3845ad8390a66ba08e7fc2c6
zdychina <zdychina@sina.cn>
2019-08-26 22:30:22 +0800

    day09

--- gmall-list-web/src/main/java/com/atguigu/gmall/list/controller/ListController.java
+++ gmall-list-web/src/main/java/com/atguigu/gmall/list/controller/ListController.java
@@ -1,7 +1,6 @@
 package com.atguigu.gmall.list.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
-import com.alibaba.fastjson.JSON;
 import com.atguigu.gmall.bean.BaseAttrInfo;
 import com.atguigu.gmall.bean.BaseAttrValue;
 import com.atguigu.gmall.bean.SkuLsParams;
@@ -12,7 +11,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.Iterator;
